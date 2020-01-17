cadce94b99c017d03399d597c5119d10fd0d1700
kishan kunal <kishankunal15@gmail.com>
2020-01-18 00:52:44 +0530

    navigation bar updated

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,8 @@
    //$query = mysqli_query($con,"INSERT INTO test VALUES(NULL,'Kunal')");
    if(isset($_SESSION['username'])){
        $userLoggedIn = $_SESSION['username'];
+       $user_details_query = mysqli_query($con,"SELECT * FROM user WHERE username='$userLoggedIn'");
+       $user = mysqli_fetch_array($user_details_query);
    }else{
        header("location:register.php");
    }
@@ -14,9 +16,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>fakebook</title>
+    <title>MeetUp</title>
+    <!--Javascript-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="assets/js/bootstrap.js"></script>
-    <link rel="stylesheet" href="assets/css/bootstrap.css.css">
+    
+    <!--css-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    
 </head>
 <body>
+     <div class="top_bar">
+        <div class="logo">
+            <a href="index.php">MeetUp !</a>
+        </div>
+        <nav>
+            <a href=""><?php echo $user['first_name']?></i></a>
+            <a href="index.php"><i class="fa fa-home fa-lg"></i></a>
+            <a href=""><i class="fa fa-envelop fa-lg"></i></a>
+            <a href=""><i class="fa fa-bell-o fa-lg"></i></a>
+            <a href=""><i class="fa fa-users fa-lg"></i></a>
+            <a href=""><i class="fa fa-cog fa-lg"></i></a>
+            <a href=""><i class="fa fa-sign-out fa-lg"></i></a>
+        </nav>
+     </div>
