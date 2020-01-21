758230d13a8fac1d12142edda0f630accea3a9b9
kishan kunal <kishankunal15@gmail.com>
2020-01-21 23:55:34 +0530

    user profile page added

--- profile.php
+++ profile.php
@@ -1,8 +1,39 @@
 <?php
     include("includes/header.php");
-?>
-   
-        <div class="user_details column">
+    include("includes/classes/User.php");
+    include("includes/classes/Post.php");
+
+    if(isset($_GET['profile_username'])){
+        $username = $_GET['profile_username'];
+        $user_details_query = mysqli_query($con, "SELECT * FROM user where username ='$username'");
+        $user_array = mysqli_fetch_array($user_details_query);
+        $num_friends = (substr_count($user_array['friend_array'],","))-1;
+    }
+?>      
+        <style type="text/css">
+	 	.wrapper {
+	 		margin-left: 0px;
+			padding-left: 0px;
+	 	}
+
+ 	</style>
+        <div class="profile_left">
+            <img src="<?php echo $user_array['profile_pic']?>">
+            <div class="profile_info">
+                <p><?php echo "Posts: ".$user_array['num_posts']; ?></p>
+                <p><?php echo "Likes: ".$user_array['num_likes']; ?></p>
+                <p><?php echo "Friends: ".$num_friends; ?></p>
+            </div>
+            <form action="<?php echo $username; ?>">
+               <?php
+               $profile_user_obj = new User($con,$username);
+               if($profile_user_obj->isClosed()){
+                   header("Location:user_closed.php");
+               }
+               ?>
+            </form>
+        </div>
+        <div class="main_details column">
             This is a profile pic
         </div>
    </div>
---
+++ user_closed.php
@@ -0,0 +1,11 @@
+<?php
+    include("includes/header.php");
+    include("includes/classes/User.php");
+    include("includes/classes/Post.php");
+?> 
+<div class="main_column column" id="main_column">
+    <h4>User Closed</h4>
+    This account is closed.
+    <a href="index.php">Click here to go back.</a>
+
+</div>    
